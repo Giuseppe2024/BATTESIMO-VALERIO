f2d442fcfc84d70871039546706294c9d0735fd2
Giuseppe2024 <venutogiuseppe@msn.com>
2024-06-04 00:47:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document is ready!');
 });
+<script>
+    function searchGuest() {
+        var input, filter, tables, table, guests, i, txtValue;
+        input = document.getElementById('searchInput');
+        filter = input.value.toUpperCase();
+        tables = document.querySelectorAll('.table');
+        
+        for (i = 0; i < tables.length; i++) {
+            table = tables[i];
+            guests = table.querySelector('h3');
+            txtValue = guests.textContent || guests.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                table.style.display = "";
+            } else {
+                table.style.display = "none";
+            }
+        }
+    }
+</script>
+
