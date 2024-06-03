f2b705c1b54b52fe74c87557cf5378fd38cc73d0
Giuseppe2024 <venutogiuseppe@msn.com>
2024-06-04 01:18:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-<script>
-    function searchGuest() {
-        var input, filter, tables, table, guests, i, txtValue;
-        input = document.getElementById('searchInput');
-        filter = input.value.toUpperCase();
-        tables = document.querySelectorAll('.table');
-
-        for (i = 0; i < tables.length; i++) {
-            table = tables[i];
-            guests = table.querySelector('h3');
-            txtValue = guests.textContent || guests.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                table.style.display = "";
-            } else {
-                table.style.display = "none";
-            }
+function searchGuest() {
+    console.log("searchGuest() function called"); // Aggiunta per il debug
+    var input, filter, tables, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    tables = document.getElementsByClassName("table");
+  
+    for (i = 0; i < tables.length; i++) {
+      table = tables[i];
+      tr = table.getElementsByTagName("h3")[0];
+      if (tr) {
+        txtValue = tr.textContent || tr.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          table.style.display = "";
+        } else {
+          table.style.display = "none";
         }
+      }       
     }
-</script>
-
-
+}
