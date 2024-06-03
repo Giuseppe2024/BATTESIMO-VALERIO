cc222f8d7bbac6519791832437f3d96ff7adbf68
Giuseppe2024 <venutogiuseppe@msn.com>
2024-06-04 01:31:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 function searchGuest() {
-    // Otteniamo il valore inserito nell'input di ricerca
-    var input = document.getElementById("searchInput").value.toUpperCase();
+    console.log("searchGuest() function called"); // Aggiunta per il debug
+    var input, filter, tables, i, j, txtValue;
+    input = document.getElementById("searchInput");
+    console.log("Input value:", input.value); // Aggiunta per il debug
+    filter = input.value.toUpperCase();
+    tables = document.getElementsByClassName("table");
+    console.log("Number of tables:", tables.length); // Aggiunta per il debug
 
-    // Otteniamo tutti gli elementi della classe "table"
-    var tables = document.getElementsByClassName("table");
-
-    // Iteriamo su ciascun elemento con classe "table"
-    for (var i = 0; i < tables.length; i++) {
-        // Otteniamo il testo all'interno del primo elemento <h3> in ogni tavolo
-        var header = tables[i].getElementsByTagName("h3")[0];
-        if (header) {
-            var txtValue = header.textContent || header.innerText;
-            // Confrontiamo il testo con il valore di ricerca
-            if (txtValue.toUpperCase().indexOf(input) > -1) {
-                // Se troviamo una corrispondenza, mostriamo il tavolo
-                tables[i].style.display = "";
-            } else {
-                // Altrimenti nascondiamo il tavolo
-                tables[i].style.display = "none";
+    for (i = 0; i < tables.length; i++) {
+        var table = tables[i];
+        var tableHeaderText = table.querySelector("h3").textContent.toUpperCase();
+        var tableRows = table.querySelectorAll("h3 ~ h3");
+        var found = false;
+        for (j = 0; j < tableRows.length; j++) {
+            txtValue = tableRows[j].textContent || tableRows[j].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+                break;
             }
+        }
+        if (found || tableHeaderText.indexOf(filter) > -1) {
+            table.style.display = "";
+        } else {
+            table.style.display = "none";
         }
     }
 }
