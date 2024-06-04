a42607d71e2c69401c6daba4eafaf6183237e972
Giuseppe2024 <venutogiuseppe@msn.com>
2024-06-04 21:37:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function searchGuest() {
 
 // Funzione per avviare il countdown
 function startCountdown() {
-    var countdownDate = new Date("Jul 1, 2024 12:00:00").getTime(); // Imposta la data corretta
+    var countdownDate = new Date("Jun 16, 2024 10:55:00").getTime(); // Imposta la data corretta
 
     countdownInterval = setInterval(function() {
         var now = new Date().getTime();
